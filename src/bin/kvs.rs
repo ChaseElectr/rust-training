b6f58ab2d0039e29e90bd8e2353f2bd3d99539bb
Qi <295872776@qq.com>
2019-04-04 21:25:40 +0800

    Section 1: part 4, complete doc comments, move run() into main().

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,3 +1,34 @@
+use clap::{App, Arg, SubCommand};
+
+fn unimpl() {
+    eprintln!("unimplemented");
+    std::process::exit(1);
+}
+
 fn main() {
-    kvs::run()
+        let matches = App::new("kvs")
+        .version(env!("CARGO_PKG_VERSION"))
+        .author("Qi <295872776@qq.com>")
+        .about("A key-value store")
+        .subcommand(
+            SubCommand::with_name("set")
+                .args(&[
+                    Arg::with_name("key").takes_value(true),
+                    Arg::with_name("value").takes_value(true),
+                ])
+                .help("Set the value of a string key to a string"),
+        )
+        .subcommand(
+            SubCommand::with_name("get")
+                .arg(Arg::with_name("key").takes_value(true))
+                .help("Get the string value of a given string key"),
+        )
+        .get_matches();
+
+    match matches.subcommand_name() {
+        Some("set") => unimpl(),
+        Some("get") => unimpl(),
+        None => std::process::exit(1),
+        _ => unreachable!(),
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,56 +1,55 @@
-use clap::{App, Arg, SubCommand};
+#![deny(missing_docs)]
+//! A simple key/value store.
+
 use std::collections::HashMap;
 
+/// The type for storing key-value pairs. The key and the value are both String, and each key must be assigned with a value.
+///
+/// You can store the key-value pair by set() method, and get a key's value by get() method. This is all we support now.
 #[derive(Default)]
 pub struct KvStore {
     store: HashMap<String, String>,
 }
 
-fn unimpl() {
-    eprintln!("unimplemented");
-    std::process::exit(1);
-}
-
 impl KvStore {
+    /// Creates a KvStore
     pub fn new() -> Self {
         KvStore {
             store: HashMap::new(),
-    }
+        }
     }
 
+    /// Store a key with it's value, this will store a key and it's value to the storage.
+    /// If the key has already been exist, the value will be overwrited.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use kvs::KvStore;
+    /// let mut store = KvStore::new();
+    /// store.set("key".to_owned(), "value1".to_owned());
+    /// assert_eq!(Some("value1".to_owned()), store.get("key".to_owned()));
+    /// store.set("key".to_owned(), "value2".to_owned());
+    /// assert_eq!(Some("value2".to_owned()), store.get("key".to_owned()));
+    /// ```
     pub fn set(&mut self, key: String, value: String) {
         self.store.insert(key, value);
     }
 
+    /// Get a key's value.
+    /// Return Some(_) if the key exists, return None otherwise.
+    /// The return value is a copy of the stored value, so it won't delete the origin data.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use kvs::KvStore;
+    /// let mut store = KvStore::new();
+    /// store.set("key".to_owned(), "value".to_owned());
+    /// assert_eq!(Some("value".to_owned()), store.get("key".to_owned()));
+    /// assert_eq!(None, store.get("kkkk".to_owned()));
+    /// ```
     pub fn get(&self, key: String) -> Option<String> {
         self.store.get(&key).cloned()
-    }
-}
-
-pub fn run() {
-    let matches = App::new("kvs")
-        .version(env!("CARGO_PKG_VERSION"))
-        .author("Qi <295872776@qq.com>")
-        .about("A key-value store")
-        .subcommand(
-            SubCommand::with_name("set")
-                .args(&[
-                    Arg::with_name("key").takes_value(true),
-                    Arg::with_name("value").takes_value(true),
-                ])
-                .help("Set the value of a string key to a string"),
-        )
-        .subcommand(
-            SubCommand::with_name("get")
-                .arg(Arg::with_name("key").takes_value(true))
-                .help("Get the string value of a given string key"),
-        )
-        .get_matches();
-
-    match matches.subcommand_name() {
-        Some("set") => unimpl(),
-        Some("get") => unimpl(),
-        None => std::process::exit(1),
-        _ => unreachable!(),
     }
 }
